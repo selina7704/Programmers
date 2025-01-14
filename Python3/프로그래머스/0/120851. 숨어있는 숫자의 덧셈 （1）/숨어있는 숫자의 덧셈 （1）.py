4a1c72d2a30e8376821cea7be102205a93dcbfbe
Selina <eunbiro@gmail.com>
2025-01-14 20:28:28 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -0,0 +1,9 @@
+def solution(my_string):
+    return sum(int(i) for i in my_string if i.isdigit())
+
+# def solution(my_string):
+#     answer = 0
+#     for i in my_string:
+#         if i.isdigit():
+#             answer += int(i)
+#     return answer
